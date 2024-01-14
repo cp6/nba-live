2bbab699ca64868c183e0a59fab34e232ad813a3
cp6 <cp6@users.noreply.github.com>
2024-01-14 22:08:28 +1100

    Updated NBATeamInfo info array

--- src/NBATeamInfo.php
+++ src/NBATeamInfo.php
@@ -21,6 +21,25 @@ class NBATeamInfo extends NBABase
 
         $this->data = $this->ApiCall("https://stats.nba.com/stats/teaminfocommon?LeagueID=00&Season=&SeasonType=&TeamID={$this->team_id}");
 
+        $info = $this->data['resultSets'][0]['rowSet'][0];
+
+        $this->info = [
+            'team_id' => $this->team_id,
+            'season' => $info[1],
+            'city' => $info[2],
+            'name' => $info[3],
+            'short_name' => $info[4],
+            'conference' => $info[5],
+            'division' => $info[6],
+            'slug' => $info[8],
+            'wins' => $info[9],
+            'loss' => $info[10],
+            'pct' => $info[11],
+            'conf_rank' => $info[12],
+            'div_rank' => $info[13],
+            'min_year' => $info[14],
+            'max_year' => $info[15],
+        ];
 
     }
 
