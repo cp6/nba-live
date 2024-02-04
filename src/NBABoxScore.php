8c176de2bc543c83989231630a013e18f7eab305
cp6 <cp6@users.noreply.github.com>
2024-02-04 21:26:47 +1100

    Error proofed NBABoxScore

--- src/NBABoxScore.php
+++ src/NBABoxScore.php
@@ -28,14 +28,16 @@ class NBABoxScore extends NBABase
 
         $this->data = $this->ApiCall("https://cdn.nba.com/static/json/liveData/boxscore/boxscore_{$this->game_id}.json");
 
-        $this->home_team = $this->data['game']['homeTeam']['statistics'];
-        $this->away_team = $this->data['game']['awayTeam']['statistics'];
+        if (isset($this->data['game'])) {
+            $this->home_team = $this->data['game']['homeTeam']['statistics'];
+            $this->away_team = $this->data['game']['awayTeam']['statistics'];
 
-        $this->home_players = $this->data['game']['homeTeam']['players'];
-        $this->away_players = $this->data['game']['awayTeam']['players'];
+            $this->home_players = $this->data['game']['homeTeam']['players'];
+            $this->away_players = $this->data['game']['awayTeam']['players'];
 
-        $this->home_tid = $this->data['game']['homeTeam']['teamId'];
-        $this->away_tid = $this->data['game']['awayTeam']['teamId'];
+            $this->home_tid = $this->data['game']['homeTeam']['teamId'];
+            $this->away_tid = $this->data['game']['awayTeam']['teamId'];
+        }
 
     }
 
